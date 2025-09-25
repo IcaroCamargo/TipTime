f2d336dc101846ade467483a481b325f16b97ed3
IcaroCamargo <icarod331@gmail.com>
2025-09-24 21:28:35 -0300

    ArredondarGorjeta

--- TipTime/MainPage.xaml.cs
+++ TipTime/MainPage.xaml.cs
@@ -2,7 +2,8 @@
 {
     public partial class MainPage : ContentPage
     {
-        private int _valor;
+        private double valorNumerico;
+        private double valorSlider;
 
         public MainPage()
         {
@@ -13,40 +14,81 @@
         private void Porcentagem15_Clicked(object sender, EventArgs e)
         {
             PorcentagemSlider.Value = 15;
-            _valor = 15;
-            ValorGorjetaLabel.Text = _valor + "%";
+            double porcento15;
+            porcento15 = valorNumerico * 0.15;
+            double valtotal;
+            valtotal = porcento15 + valorNumerico;
+            PorcentagemGorjetaLabel.Text = PorcentagemSlider.Value + "%";
+            ValorGorjetaLabel.Text = porcento15.ToString();
+            ValorTotalLabel.Text = valtotal.ToString();
+
 
         }
 
         private void Porcentagem20_Clicked(object sender, EventArgs e)
         {
             PorcentagemSlider.Value = 20;
-            _valor = 20;
-            ValorGorjetaLabel.Text = _valor + "%";
+            double porcento20;
+            porcento20 = valorNumerico * 0.2;
+            double valtotal;
+            valtotal = porcento20 + valorNumerico;
+            PorcentagemGorjetaLabel.Text = PorcentagemSlider.Value + "%";
+            ValorGorjetaLabel.Text = porcento20.ToString();
+            ValorTotalLabel.Text = valtotal.ToString();
         }
 
         private void ArredondamentoAcima_Clicked(object sender, EventArgs e)
         {
-
+            PorcentagemSlider.Value = Math.Ceiling(PorcentagemSlider.Value);
         }
 
         private void ArredondamentoAbaixo_Clicked(object sender, EventArgs e)
         {
-
+            PorcentagemSlider.Value = Math.Floor(PorcentagemSlider.Value);
         }
 
         private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
+            valorSlider = e.NewValue;
+            double valgorjeta, valtotal;
+            valgorjeta = valorNumerico * (valorSlider / 100);
+            valtotal = valorNumerico + valgorjeta;
+            PorcentagemGorjetaLabel.Text = $"{valorSlider:F1}%";
+            ValorTotalLabel.Text =(valtotal.ToString("F1"));
+            ValorGorjetaLabel.Text = (valgorjeta.ToString("F1"));
 
-             
         }
 
         private void ValorTotalEntry_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (double.TryParse(e.NewTextValue, out double numero))
-            
-                ValorTotalLabel.Text = numero.ToString();
-            
+            if (int.TryParse(e.NewTextValue, out int numero))
+            {
+                valorNumerico = numero;
+            }
+            else
+            {
+                valorNumerico = 0;
+            }
+        }
+
+        private void ArredondarAcimaValorTotal_Clicked(object sender, EventArgs e)
+        {
+            double valgorjeta, valtotal;
+            valgorjeta = valorNumerico * (valorSlider / 100);
+            valtotal = valorNumerico + valgorjeta;
+            double arac = Math.Ceiling(valtotal);
+            ValorGorjetaLabel.Text = (arac - valorNumerico).ToString();
+            ValorTotalLabel.Text = arac.ToString();
+        }
+
+        private void ArredondarAbaixoValorTotal_Clicked(object sender, EventArgs e)
+        {
+            double valgorjeta, valtotal;
+            valgorjeta = valorNumerico * (valorSlider / 100);
+            valtotal = valorNumerico + valgorjeta;
+            double arac = Math.Floor(valtotal);
+            ValorGorjetaLabel.Text = (arac - valorNumerico).ToString();
+            ValorTotalLabel.Text = arac.ToString();
         }
     }
 
